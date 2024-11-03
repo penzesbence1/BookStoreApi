59d6f01c67104f20acdd8be239ed6fa076839dce
penzesbence1 <pebex0604@gmail.com>
2024-11-03 16:57:04 +0100

    Configure JSON serialization options

--- Program.cs
+++ Program.cs
@@ -17,7 +17,10 @@ namespace BookStoreApi
             builder.Services.AddSingleton<BooksService>();
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(
+                options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
